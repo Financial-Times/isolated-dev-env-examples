ed9b59c31b6225319371b70ba34504fbc018f556
robgodfrey <rob.godfrey@ft.com>
2023-10-31 11:03:40 +0000

    Encapsulate python and doppler init into functions

--- examples/python/devbox_init.sh
+++ examples/python/devbox_init.sh
@@ -1,24 +1,40 @@
-echo ""
-python --version
-pip --version
+init_python () {
+  echo ""
+  python --version
+  pip --version
+
+  echo ""
+  echo "Activating python virtual env:"
+  echo $VENV_DIR
+  source $VENV_DIR/bin/activate
+
+  echo ""
+  echo "Installing python requirements:"
+  python -m pip install -r requirements.txt
+}
+
+init_doppler () {
+  echo ""
+  doppler run -- true  2> /dev/null
+  if [ $? -ne 0 ]; then
+    echo "Authenticate with Doppler:"
+    doppler login
+  fi
+  if [ -z $(doppler configure get config --plain) ]; then
+    echo "Configure Doppler config:"
+    doppler setup -p isolated-dev-env-examples
+  fi
+  echo "Configuring Doppler secrets as env vars:"
+  echo "  project: $(doppler configure get project --plain)"
+  echo "  config:  $(doppler configure get config --plain)"
+  source <(doppler secrets download --no-file --format env)
+}
 
 echo ""
-echo "Activating python virtual env:"
-echo $VENV_DIR
-source $VENV_DIR/bin/activate
+echo "Executing $(basename $0) ..."
+
+init_python
+init_doppler
 
 echo ""
-echo "Installing python requirements:"
-pip install -r requirements.txt
-
-echo ""
-echo "Setting up Doppler secrets:"
-doppler run -- true  2> /dev/null
-if [ $? -ne 0 ]; then
-  doppler login
-fi
-if [ -z $(doppler configure get config --plain) ]; then
-  doppler setup -p isolated-dev-env-examples
-fi
-source <(doppler secrets download --no-file --format env)
 echo "Done."
