44c9f01c344d6b12e99f55ffb0bc6450b48cbaed
robgodfrey <rob.godfrey@ft.com>
2023-10-31 15:15:37 +0000

    Add sls example

---
+++ examples/sls/devbox_init.sh
@@ -0,0 +1,47 @@
+init_python () {
+  echo ""
+  echo "Python version: $(python --version)"
+  echo "Pip version:    $(pip --version)"
+
+  echo ""
+  echo "Activating python virtual env:"
+  echo $VENV_DIR
+  source $VENV_DIR/bin/activate
+
+  echo ""
+  echo "Installing python requirements:"
+  python -m pip install -r requirements.txt
+}
+
+init_sls() {
+  echo ""
+  echo "Serverless version: $(sls --version)"
+}
+
+init_doppler () {
+  echo ""
+  echo "Doppler version: $(doppler --version)"
+  doppler run -- true  2> /dev/null
+  if [ $? -ne 0 ]; then
+    echo "Authenticate with Doppler:"
+    doppler login
+  fi
+  if [ -z $(doppler configure get config --plain) ]; then
+    echo "Configure Doppler config:"
+    doppler setup -p isolated-dev-env-examples
+  fi
+  echo "Configuring Doppler secrets as env vars:"
+  echo "  project: $(doppler configure get project --plain)"
+  echo "  config:  $(doppler configure get config --plain)"
+  source <(doppler secrets download --no-file --format env)
+}
+
+echo ""
+echo "Executing $(basename $0) ..."
+
+init_python
+init_sls
+init_doppler
+
+echo ""
+echo "Done."
