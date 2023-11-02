0e7d17d2847e4b2442da7f7193baee847c0ecb4a
robgodfrey <rob.godfrey@ft.com>
2023-11-02 12:46:51 +0000

    Extracted doppler plugin

--- examples/node/devbox_init.sh
+++ examples/node/devbox_init.sh
@@ -7,31 +7,10 @@ init_node() {
   npm install
 }
 
-init_doppler () {
-  echo ""
-  echo "Doppler version: $(doppler --version)"
-  doppler run -- true  2> /dev/null
-  if [ $? -ne 0 ]; then
-    echo "Authenticate with Doppler:"
-    doppler login
-  fi
-  if [ -z $(doppler configure get config --plain) ]; then
-    echo "Configure Doppler config:"
-    doppler setup -p $DOPPLER_PROJECT
-  fi
-  echo "Configuring Doppler secrets as env vars:"
-  echo "  project: $(doppler configure get project --plain)"
-  echo "  config:  $(doppler configure get config --plain)"
-  source <(doppler secrets download --no-file --format env)
-}
-
 echo ""
 echo "Executing $(basename $0) ..."
 
 init_node
-
-DOPPLER_PROJECT="repo_isolated-dev-env-examples"
-init_doppler
 
 echo ""
 echo "Done."
