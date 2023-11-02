2d806df8aaa098aa8f504621a80dc2eec510237e
robgodfrey <rob.godfrey@ft.com>
2023-11-02 12:45:43 +0000

    Extracted doppler plugin

--- examples/sls/devbox_init.sh
+++ examples/sls/devbox_init.sh
@@ -18,32 +18,11 @@ init_sls() {
   echo "Serverless version: $(sls --version)"
 }
 
-init_doppler () {
-  echo ""
-  echo "Doppler version: $(doppler --version)"
-  doppler run -- true  2> /dev/null
-  if [ $? -ne 0 ]; then
-    echo "Authenticate with Doppler:"
-    doppler login
-  fi
-  if [ -z $(doppler configure get config --plain) ]; then
-    echo "Configure Doppler config:"
-    doppler setup -p $DOPPLER_PROJECT
-  fi
-  echo "Configuring Doppler secrets as env vars:"
-  echo "  project: $(doppler configure get project --plain)"
-  echo "  config:  $(doppler configure get config --plain)"
-  source <(doppler secrets download --no-file --format env)
-}
-
 echo ""
 echo "Executing $(basename $0) ..."
 
 init_python
 init_sls
-
-DOPPLER_PROJECT="repo_isolated-dev-env-examples"
-init_doppler
 
 echo ""
 echo "Done."
